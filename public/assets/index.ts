9bf9920c37b39e8793c90b5034dd26d182bc2954
ilyanosovsky <imnosovsky@gmail.com>
2023-11-21 22:53:42 +0200

    need to update info

---
+++ public/assets/index.ts
@@ -0,0 +1,15 @@
+// import logo from "./images/logo.png";
+// import profileImg from "./images/profile.png";
+// import courtImg from "./images/court-app.png";
+// import projectsImg from "./images/mini-projects.png";
+// import travelImg from "./images/travelmap.png";
+
+// export { logo, profileImg, courtImg, projectsImg, travelImg };
+
+import logo from "./images/logo.png";
+import profileImg from "./images/profile.png";
+import amazonImg from "./images/amazonImg.png";
+import cyberImg from "./images/cyberBlog.png";
+import noorShop from "./images/noorShop.png";
+
+export { logo, profileImg, amazonImg, cyberImg, noorShop };
