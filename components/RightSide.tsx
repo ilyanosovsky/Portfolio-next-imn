3966937b6d2431df590079b2c387d23694223091
ilyanosovsky <imnosovsky@gmail.com>
2023-11-22 20:56:54 +0200

    updated right side line

--- components/RightSide.tsx
+++ components/RightSide.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const RightSide = () => {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-end gap-6 text-textLight">
+    <div className="w-full h-full flex flex-col items-center justify-end gap-12 text-textLight">
       <a href="mailto:imnosovsky@gmail.com">
         <p className="text-sm rotate-90 w-72 tracking-widest text-textGreen">
           imnosovsky@gmail.com
