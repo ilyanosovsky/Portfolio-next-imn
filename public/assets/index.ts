2c23b908522946382e59e9c15c4eeb1d1ca9f975
ilyanosovsky <imnosovsky@gmail.com>
2023-12-24 21:06:46 +0200

    updated projects

--- public/assets/index.ts
+++
@@ -1,7 +0,0 @@
-import logo from "./images/logo.png";
-import profileImg from "./images/profile.png";
-import courtImg from "./images/court.png";
-import threadsImg from "./images/threads.png";
-import travelImg from "./images/travelmap.png";
-
-export { logo, profileImg, courtImg, threadsImg, travelImg };
