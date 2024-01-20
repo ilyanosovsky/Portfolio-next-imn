d3c3837104c4d92f782e03647cde90d38b1d57dd
ilyanosovsky <imnosovsky@gmail.com>
2024-01-20 14:34:30 +0200

    changed name

--- components/works/PIKModul.tsx
+++ components/works/PIKModul.tsx
@@ -9,7 +9,7 @@ const PIKModul = () => {
       className="w-full"
     >
       <h3 className="flex gap-1 font-medium text-xl font-titleFont">
-        Data Analyst
+        Web Developer
         <span className="text-textGreen tracking-wide">@PIK-Modul</span>
       </h3>
       <p className="text-sm mt-1 font-medium text-textDark">
