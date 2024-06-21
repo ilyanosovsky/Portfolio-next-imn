b57384e22f0e93fcabd7815fe05ff8111b65c28a
ilyanosovsky <imnosovsky@gmail.com>
2024-06-21 19:24:48 +0300

    fixing mobile version

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -236,7 +236,7 @@ const Navbar = () => {
                     </motion.li>
                   </Link>
                 </ul>
-                <a href="/assets/CV_ilya_nosovsky.pdf" target="_blank">
+                <a href="/assets/CV_ilya_Nosovsky_FullStack.pdf" target="_blank">
                   <motion.button
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -14,8 +14,8 @@ const Projects = () => {
       <SectionTitle title="Some Things I have Built" titleNo="03" />
 
       <div className="w-full flex flex-col items-center justify-center gap-28 mt-10">
-        {/* ============ project One Start here ============== */}
-        <div className="flex flex-col xl:flex-row-reverse gap-6">
+        {/* ============ project One Start here ================ */}
+        <div className="flex flex-col xl:flex-row gap-6">
           <a
             className="w-full xl:w-1/2 h-auto relative group"
             href="https://order-eats-app.onrender.com/"
@@ -23,35 +23,34 @@ const Projects = () => {
           >
             <div>
               <Image
-                className="w-full h-full object-contain"
+                className="w-full h-full object-contain rounded-lg"
                 src={orderEatsImg}
                 alt="orderEatsImg"
               />
               <div className="absolute w-full h-full bg-textGreen/10 rounded-lg top-0 left-0 group-hover:bg-transparent duration-300"></div>
             </div>
           </a>
-          <div className="w-full xl:w-1/2 flex flex-col gap-6 justify-between items-end text-right xl:-ml-16 z-10">
+          <div className="w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-between items-end text-right xl:-ml-16 z-10">
             <div>
               <p className="font-titleFont text-textGreen text-sm tracking-wide">
                 Featured Project
               </p>
               <h3 className="text-2xl font-bold">Order Eats</h3>
             </div>
-            <p className="text-sm md:text-base bg-[#112240] p-2 md:p-6 rounded-md">
+            <p className="bg-[#112240] text-sm md:text-base p-2 md:p-6 rounded-md">
               Order Eats is a{" "}
               <span className="text-textGreen">food-ordering platform</span>{" "}
               that provides users with a seamless and enjoyable experience. The
               app includes features like user authentication, restaurant
               management, cart operations, real-time order tracking, and more.
               We leverage modern technologies and frameworks to deliver a robust
-              and scalable solution.
+              and scalable solution.{" "}
             </p>
             <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
               <li>ReactJS</li>
               <li>MongoDB</li>
               <li>TailwindCSS</li>
-              <li>Shadcn/UI</li>
-              <li>TypeScript</li>
+              <li>TS</li>
               <li>Vite</li>
               <li>Stripe</li>
               <li>Auth0</li>
@@ -78,63 +77,6 @@ const Projects = () => {
         </div>
         {/* ============ project One End here ================== */}
         {/* ============ project Two Start here ================ */}
-        <div className="flex flex-col xl:flex-row gap-6">
-          <a
-            className="w-full xl:w-1/2 h-auto relative group"
-            href="https://court-app.onrender.com/"
-            target="_blank"
-          >
-            <div>
-              <Image
-                className="w-full h-full object-contain"
-                src={courtImg}
-                alt="courtImg"
-              />
-              <div className="absolute w-full h-full bg-textGreen/10 rounded-lg top-0 left-0 group-hover:bg-transparent duration-300"></div>
-            </div>
-          </a>
-          <div className="w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-between items-end text-right xl:-ml-16 z-10">
-            <div>
-              <p className="font-titleFont text-textGreen text-sm tracking-wide">
-                Featured Project
-              </p>
-              <h3 className="text-2xl font-bold">Court App</h3>
-            </div>
-            <p className="bg-[#112240] text-sm md:text-base p-2 md:p-6 rounded-md">
-              The Court web app allows players to find a tennis sparring partner
-              on a court near you. A kind of{" "}
-              <span className="text-textGreen">social network</span> for tennis
-              players. Includes a map with courts, the ability to add to
-              friends, create posts with the offer to play.{" "}
-            </p>
-            <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
-              <li>ReactJS</li>
-              <li>Redux</li>
-              <li>MongoDB</li>
-              <li>NodeJS</li>
-              <li>Cloudinary</li>
-              <li>OnRender</li>
-            </ul>
-            <div className="text-2xl flex gap-4">
-              <a
-                className="hover:text-textGreen duration-300"
-                href="https://github.com/ilyanosovsky/Court_App"
-                target="_blank"
-              >
-                <TbBrandGithub />
-              </a>
-              <a
-                className="hover:text-textGreen duration-300"
-                href="https://court-app.onrender.com/"
-                target="_blank"
-              >
-                <RxOpenInNewWindow />
-              </a>
-            </div>
-          </div>
-        </div>
-        {/* ============ project Two End here ================== */}
-        {/* ============ project Three Start here ================ */}
         <div className="flex flex-col xl:flex-row-reverse gap-6">
           <a
             className="w-full xl:w-1/2 h-auto relative group"
@@ -143,7 +85,7 @@ const Projects = () => {
           >
             <div>
               <Image
-                className="w-full h-full object-contain"
+                className="w-full h-full object-contain rounded-lg"
                 src={threadsImg}
                 alt="threadsImg"
               />
@@ -183,6 +125,64 @@ const Projects = () => {
               <a
                 className="hover:text-textGreen duration-300"
                 href="https://threads-clone-five-lac.vercel.app/"
+                target="_blank"
+              >
+                <RxOpenInNewWindow />
+              </a>
+            </div>
+          </div>
+        </div>
+        {/* ============ project Two End here ================== */}
+
+        {/* ============ project Three Start here ================ */}
+        <div className="flex flex-col xl:flex-row gap-6">
+          <a
+            className="w-full xl:w-1/2 h-auto relative group"
+            href="https://court-app.onrender.com/"
+            target="_blank"
+          >
+            <div>
+              <Image
+                className="w-full h-full object-contain rounded-lg"
+                src={courtImg}
+                alt="courtImg"
+              />
+              <div className="absolute w-full h-full bg-textGreen/10 rounded-lg top-0 left-0 group-hover:bg-transparent duration-300"></div>
+            </div>
+          </a>
+          <div className="w-full xl:w-1/2 flex flex-col gap-6 lgl:justify-between items-end text-right xl:-ml-16 z-10">
+            <div>
+              <p className="font-titleFont text-textGreen text-sm tracking-wide">
+                Featured Project
+              </p>
+              <h3 className="text-2xl font-bold">Court App</h3>
+            </div>
+            <p className="bg-[#112240] text-sm md:text-base p-2 md:p-6 rounded-md">
+              The Court web app allows players to find a tennis sparring partner
+              on a court near you. A kind of{" "}
+              <span className="text-textGreen">social network</span> for tennis
+              players. Includes a map with courts, the ability to add to
+              friends, create posts with the offer to play.{" "}
+            </p>
+            <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
+              <li>ReactJS</li>
+              <li>Redux</li>
+              <li>MongoDB</li>
+              <li>NodeJS</li>
+              <li>Cloudinary</li>
+              <li>OnRender</li>
+            </ul>
+            <div className="text-2xl flex gap-4">
+              <a
+                className="hover:text-textGreen duration-300"
+                href="https://github.com/ilyanosovsky/Court_App"
+                target="_blank"
+              >
+                <TbBrandGithub />
+              </a>
+              <a
+                className="hover:text-textGreen duration-300"
+                href="https://court-app.onrender.com/"
                 target="_blank"
               >
                 <RxOpenInNewWindow />
